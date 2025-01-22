997203cd0357db25e8dc31d6dfe292b6cc0c102a
glitterpillz <karenhickey1010@gmail.com>
2025-01-22 13:43:47 -0500

    deploy adjustments

--- migrations/env.py
+++ migrations/env.py
@@ -109,7 +109,7 @@ def run_migrations_online():
 
         with context.begin_transaction():
             if environment == "production":
-                connection.execute(f"CREATE SCHEMA IF NOT EXISTS {pokenote_schema};")
+                connection.execute(f"CREATE SCHEMA IF NOT EXISTS {SCHEMA};")
                 context.execute(f"SET search_path TO {SCHEMA};")
             context.run_migrations()
 
